0a5e1858c5cc97c744bf4d8de7f732ba877101c7
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-12-04 20:26:50 +0530

    journal writing page completed

--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -1,9 +1,160 @@
-import React from 'react'
+"use client";
+
+import dynamic from "next/dynamic";
+import { Controller, useForm } from "react-hook-form";
+import "react-quill-new/dist/quill.snow.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { journalSchema } from "@/app/lib/schemas";
+import { BarLoader } from "react-spinners";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { getMoodById, MOODS } from "@/app/lib/moods";
+import { Button } from "@/components/ui/button";
+
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 const JournalWritePage = () => {
-  return (
-    <div>JournalWritePage</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+    getValues
+  } = useForm({
+    resolver: zodResolver(journalSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      mood: "",
+      collectionId: "",
+    },
+  });
 
-export default JournalWritePage
+  const isLoading = false;
+
+  const onSubmit = handleSubmit( async (data) => {
+    console.log(data);
+    
+  })
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <form className="space-y-2 mx-auto" onSubmit={onSubmit}>
+        <h1 className="text-5xl md:text-6xl gradient-title">
+          What&apos;s on your mind?
+        </h1>
+
+        {isLoading && <BarLoader color="orange" width={"100%"} />}
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Title</label>
+          <Input
+          disabled={isLoading}
+            {...register("title")}
+            placeholder="Give your entry a title..."
+            className={`py-5 md:text-md ${
+              errors.title ? "border-red-500" : ""
+            }`}
+          />
+          {errors.title && (
+            <p className="text-red-500 text-sm">{errors.title.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">How are you fealing?</label>
+          <Controller
+            name="mood"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Select onValueChange={field.onChange} value={field.value}>
+                  <SelectTrigger
+                    className={errors.mood ? "border-red-500" : ""}
+                  >
+                    <SelectValue placeholder="What's your mood?" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {Object.values(MOODS).map((mood) => {
+                      return (
+                        <SelectItem key={mood.id} value={mood.id}>
+                          <span className="flex items-center gap-2">
+                            {mood.emoji} {mood.label}
+                          </span>
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              );
+            }}
+          />
+          {errors.mood && (
+            <p className="text-red-500 text-sm">{errors.mood.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">
+            {getMoodById(getValues("mood"))?.prompt ?? "write your thoughts..."}
+            </label>
+          <Controller
+            name="content"
+            control={control}
+            render={({field}) => (
+              <ReactQuill 
+              readOnly={isLoading}
+              theme="snow"
+              value={field.value}
+              onChange={field.onChange}
+              modules={{
+                toolbar: [
+                  [{ header: [1, 2, 3, false] }],
+                  ["bold", "italic", "underline", "strike"],
+                  [{ list: "ordered" }, { list: "bullet" }],
+                  ["blockquote", "code-block"],
+                  ["link"],
+                  ["clean"],
+                ],
+              }}
+              />
+            )}
+          />
+          {errors.content && (
+            <p className="text-red-500 text-sm">{errors.content.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">
+              Add to Collection (Optional)
+            </label>
+          {/* <Controller
+            name="content"
+            control={control}
+            render={({field}) => (
+             
+            )}
+          /> */}
+          {errors.collectionId && (
+            <p className="text-red-500 text-sm">{errors.collectionId.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-4 flex">
+          <Button type="submit" variant="journal">Publish</Button>
+
+        </div>
+
+      </form>
+    </div>
+  );
+};
+
+export default JournalWritePage;
